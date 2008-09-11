65d09b51ed765691af43c771c17237bceb671786
Denis Martinez <deuns.martinez@gmail.com>
2008-09-11 18:32:54 +0000

    Fixed the UNIX home dir, which now uses $HOME Stripped trailing CRs

--- src/portable/SaveToFile/savetofile-dir.cpp
+++ src/portable/SaveToFile/savetofile-dir.cpp
@@ -1,15 +1,15 @@
 #include <string>
 #include <iostream>
-#include <stdlib.h>
+#include <stdlib.h>
 #if defined(WIN32) || defined(WIN64)
 	std::string dir()
 	{
 		std::string userprofile=getenv("USERPROFILE");
 		return userprofile+"/ppasskeeper/";
 	}
-#else	std::string dir()
+#else	std::string dir()
 	{
-		std::string logname=getenv("LOGNAME");
-		return "/home/"+logname+"/.ppasskeeper/";
+		std::string home=getenv("HOME");
+		return home + "/.ppasskeeper/";
 	}
 #endif

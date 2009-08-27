37927043ca6fb256c4f320c2c7d13cdcb8f78ffb
Denis Martinez <deuns.martinez@gmail.com>
2009-08-27 06:39:47 +0000

    more work on python

--- python/binding.cpp
+++ python/binding.cpp
@@ -7,7 +7,14 @@ BOOST_PYTHON_MODULE(ppasskeeper)
 	register_exception_translator<PPassKeeperError>(&translate);
 	class_<ModuleList>("ModuleList", init<>())
 		.def("__len__", &ModuleList::length)
-		.def("__getitem__", &ModuleList::getitem)
+		.def("__getitem__", &ModuleList::getitem, return_value_policy<manage_new_object>())
+		.def("__contains__", &ModuleList::contains)
+		;
+	class_<Module>("Module", init<std::string>())
+		.def(self_ns::str(self))
+		.def("read_flags", &Module::read_flags)
+		.def("write_flags", &Module::write_flags)
+		.def("listing_flags", &Module::listing_flags)
 		;
 	def("is_locked", &is_locked);
 	def("set_password", &set_password);
--- python/module.h
+++ python/module.h
@@ -8,9 +8,30 @@ using namespace boost::python;
 
 #include "exception.h"
 
-class ModuleList
+#include <iostream>
+
+struct Module
 {
-public:
+	Module(const std::string& id) : id(id) {}
+	std::string id;
+
+	template <ppk_error (*func)(const char* module_id, unsigned int* flags)>
+	unsigned int get_flags()
+	{
+		unsigned int flags;
+		ppk_error err = func(id.c_str(), &flags);
+		if (err != PPK_OK)
+			throw PPassKeeperError(err);
+		return flags;
+	}
+	unsigned int read_flags() { return get_flags<ppk_module_read_flags>(); }
+	unsigned int write_flags() { return get_flags<ppk_module_write_flags>(); }
+	unsigned int listing_flags() { return get_flags<ppk_module_listing_flags>(); }
+};
+std::ostream& operator<<(std::ostream& s, const Module& m) { return s << m.id; }
+
+struct ModuleList
+{
 	ModuleList()
 		: m_length(-1)
 	{
@@ -23,8 +44,6 @@ public:
 	{
 		if (m_length != -1)
 			return m_length;
-		if (m_list == NULL)
-			m_length = 0;
 		else
 		{
 			m_length = 0;
@@ -34,16 +53,25 @@ public:
 		return m_length;
 	};
 
-	const char* getitem(int index)
+	Module *getitem(int index)
 	{
 		if (index < 0 || index >= length())
 		{
 			PyErr_SetObject(PyExc_IndexError, Py_None);
 			throw error_already_set();
 		}
-		return m_list[index];
+		char* id = m_list[index];
+		return new Module(id);
 	}
-private:
+
+	bool contains(const std::string& id)
+	{
+		for (char** cur = m_list; *cur != NULL; ++cur)
+			if (id == *cur)
+				return true;
+		return false;
+	}
+
 	char** m_list;
 	int m_length;
 };

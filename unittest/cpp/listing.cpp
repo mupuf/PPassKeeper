afce8118891b964c3f56ab078f77306a95c7879e
Denis Martinez <deuns.martinez@gmail.com>
2009-09-12 13:15:47 +0000

    listing test update

--- unittest/cpp/listing.cpp
+++ unittest/cpp/listing.cpp
@@ -39,6 +39,32 @@ void delete_lists()
 	if (whole_list) { ppk_module_free_entry_list(whole_list); whole_list = NULL; }
 }
 
+size_t count_for_entry_type(ppk_entry_type type)
+{
+	switch (type)
+	{
+	case ppk_network:
+		return network_count;
+	case ppk_application:
+		return application_count;
+	case ppk_item:
+		return item_count;
+	}
+}
+
+ppk_entry** list_for_entry_type(ppk_entry_type type)
+{
+	switch (type)
+	{
+	case ppk_network:
+		return network_list;
+	case ppk_application:
+		return application_list;
+	case ppk_item:
+		return item_list;
+	}
+}
+
 void list_all()
 {
 	network_count = 0;
@@ -71,6 +97,30 @@ void run(int argc, char** argv)
 
 	data=ppk_string_data_new(TEST_STRING);
 
-	list_all();
+	for (int i = 0; i < entries.size(); ++i)
+	{
+		ppk_entry* entry = entries[i];
+
+		// get the list before adding entry
+		list_all();
+		size_t prev_count = count;
+		size_t prev_count_same_type = count_for_entry_type(entry->type);
+
+		// add entry
+		ASSERT(ppk_module_set_entry(module_id, entry, data, 0) == PPK_OK);
+
+		// get the new list, check counts
+		list_all();
+		size_t count_same_type = count_for_entry_type(entry->type);
+		ppk_entry** list = list_for_entry_type(entry->type);
+		bool ok_listing = list != NULL;
+		ASSERT(ok_listing);
+		ASSERT(ok_listing && prev_count + 1 == count);
+		ASSERT(ok_listing && prev_count_same_type + 1 == count_same_type);
+
+		// remove
+		ASSERT(ppk_module_remove_entry(module_id, entry, 0) == PPK_OK);
+	}
+
 	delete_all();
 }
